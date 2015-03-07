f569edd3340060f2af967e992bce99a9b1303c29
DraperStudio <hello@draperstud.io>
2015-03-07 12:01:40 +0100

    Update comments

--- src/TwitchExtendSocialite.php
+++ src/TwitchExtendSocialite.php
@@ -6,7 +6,9 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 class TwitchExtendSocialite
 {
     /**
-     * Execute the provider.
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
      */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
